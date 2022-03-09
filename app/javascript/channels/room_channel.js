7f0ca47a157e10a66a2c0857b8c04a2da2db49ba
Ikko-T <skyblueboy32@gmail.com>
2022-03-09 16:14:41 +0900

    Implement mutual communication

---
+++ app/javascript/channels/room_channel.js
@@ -0,0 +1,22 @@
+import consumer from './consumer';
+
+document.addEventListener('turbolinks:load', () => {
+  window.messageContainer = document.getElementById('message-container');
+
+  if (messageContainer === null) {
+    return;
+  }
+
+  consumer.subscriptions.create('RoomChannel', {
+    connected() {},
+
+    disconnected() {
+      // Called when the subscription has been terminated by the server
+    },
+
+    received(data) {
+      // Called when there's incoming data on the websocket for this channel
+      messageContainer.insertAdjacentHTML('beforeend', data['message']);
+    },
+  });
+});
