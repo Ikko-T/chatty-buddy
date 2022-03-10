264f5f96c48190644121562d20f6a20d2c9f94c7
Ikko-T <skyblueboy32@gmail.com>
2022-03-10 19:10:50 +0900

    Add a another new routing

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,4 +3,8 @@ class RoomsController < ApplicationController
     @messages = Message.includes(:user).order(:id)
     @message = current_user.messages.build
   end
+
+  def show_additionally
+
+  end
 end
