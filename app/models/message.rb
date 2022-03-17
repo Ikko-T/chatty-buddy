d60116c8ee9e14c491d15ed584f5f2a3ae30d0da
Ikko-T <skyblueboy32@gmail.com>
2022-03-17 13:53:33 +0900

    Finish refactoring

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,6 +4,8 @@ class Message < ApplicationRecord
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 500 }
 
+  # scope :load_old_messages, ->(number) { Message.includes(:user).order(:id).where(id: 1..last_id).last(50) }
+
   def template
     ApplicationController.render_with_signed_in_user(user, partial: 'messages/message', locals: { message: self })
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'rooms#show'
   resources :messages, only: :create
-  get '/load_up', to: 'rooms#load_up'
+  # get '/load_up', to: 'rooms#load_up'
 end
