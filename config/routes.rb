b4951f213ac378e0d8514eb12d4186623033d8ab
Ikko-T <skyblueboy32@gmail.com>
2022-07-13 08:31:03 +0900

    Able to get a guest user account

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   mount ActionCable.server => '/cable'
   devise_for :users
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
   root 'rooms#show'
   resources :messages, only: :create
   # get '/load_up', to: 'rooms#load_up'
