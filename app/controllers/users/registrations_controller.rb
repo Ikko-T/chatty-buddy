66e979ef63def6d0b7cf1249ba3db8bc2785ed97
Ikko-T <skyblueboy32@gmail.com>
2022-07-16 18:04:55 +0900

    A guest user's account cannot be deleted

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,3 +1,9 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :
+  before_action :ensure_guest_user, only: :destroy
+
+  def ensure_guest_user
+    if resource.email == "guest@example.com"
+      redirect_to root_path, alert: "A guest user can't be deleted."
+    end
+  end
 end
