c541124867dd795b86f97c9ce8b0b81beae39db1
Ikko-T <skyblueboy32@gmail.com>
2022-07-18 11:26:01 +0900

    Disable to reset a guest user's password

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   mount ActionCable.server => '/cable'
   devise_for :users, controllers: {
-    registrations: "users/registrations"
+    registrations: "users/registrations",
+    passwords: "users/passwords"
   }
   devise_scope :user do
     post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
